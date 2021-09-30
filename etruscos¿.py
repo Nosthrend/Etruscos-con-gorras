05d99499c29c9cafe5bd0c8057d5fffdc78345c8
Joan Castells Appriou <Castells.Joan.30@gmail.com>
2021-09-30 16:26:55 +0200

    Creado el archivo etruscos¿.py
    Se ha desvelado la verdad

---
+++ etruscos¿.py
@@ -0,0 +1,28 @@
+print("Este es un programa de concienciación histórica.")
+
+etruscos=input("Una pregunta, aprendiz. ¿Los etruscos tenían gorras? Responde Sí o No: ")
+
+if etruscos="No"
+    etruscos=input("Una pregunta, aprendiz. ¿Los etruscos tenían gorras? Responde Sí o No: ")
+
+    if etruscos="No"
+        etruscos=input("Una pregunta, aprendiz. ¿Los etruscos tenían gorras? Responde Sí o No: ")
+
+        if etruscos="No"
+            etruscos=input("Una pregunta, aprendiz. ¿Los etruscos tenían gorras? Responde Sí o No: ")
+
+            if etruscos="No"
+                etruscos=input("Una pregunta, aprendiz. ¿Los etruscos tenían gorras? Responde Sí o No: ")
+
+            else
+                print("¡Felicidades! Eres inteligente, y para nada influenciable."
+
+        else
+            print("¡Felicidades! Eres inteligente, y para nada influenciable."
+
+    else
+        print("¡Felicidades! Eres inteligente, y para nada influenciable."
+
+
+else
+    print("¡Felicidades! Eres inteligente, y para nada un crédulo."
